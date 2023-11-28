d46663e361c6e0d4a520e2e0120da3d28b7c47f8
Artem <artem.semenov.101@gmail.com>
2023-11-28 22:07:05 +0200

    added business

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import mobileNav from "./modules/mobile-nav.js";
 import { Forms } from "./modules/Forms.js";
 import { collectionsTabs } from "./modules/collectionsTabs.js";
+import { faqAccordion } from "./modules/faq.js";
 document.addEventListener("DOMContentLoaded", (e) => {
   mobileNav();
   collectionsTabs();
+  faqAccordion();
   const forms = new Forms();
 });
