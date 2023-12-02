6b1ff630f11e5d63aa947a0fe851013bfb8146d9
Artem <artem.semenov.101@gmail.com>
2023-12-02 20:49:04 +0200

    finish want

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,10 @@ import { Forms } from "./modules/Forms.js";
 import { collectionsTabs } from "./modules/collectionsTabs.js";
 import { faqAccordion } from "./modules/faq.js";
 import { SmoothAnchorScroll } from "./modules/smoothSCroll.js";
+import Masonry from "masonry-layout";
+
+import imagesLoaded from "imagesloaded";
+
 document.addEventListener("DOMContentLoaded", (e) => {
   mobileNav();
   collectionsTabs();
@@ -11,4 +15,16 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const smoothAnchorScroll = new SmoothAnchorScroll();
   smoothAnchorScroll.init();
   const forms = new Forms();
+
+  const wantGalelry = document.querySelector(".wantHero__gallery");
+  if (wantGalelry) {
+    imagesLoaded(wantGalelry, () => {
+      window.msnry = new Masonry(wantGalelry, {
+        itemSelector: ".wantHero__gallery-item",
+        percentPosition: true,
+        columnWidth: 1,
+        horizontalOrder: true,
+      });
+    });
+  }
 });
