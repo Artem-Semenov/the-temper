40eef0454fc6f8c7fd712e9ce49f379750d7f531
Artem <artem.semenov.101@gmail.com>
2024-01-03 19:37:06 +0200

    update skittles active collections

--- src/js/modules/3dScenes.js
+++ src/js/modules/3dScenes.js
@@ -2,21 +2,22 @@ export function objectScenes() {
   const scenes = document.querySelectorAll(".active-collections__item");
   if (!scenes) return;
   scenes.forEach((scene) => {
-    const sceneIndex = scene.dataset.scene;
+    const sceneTitle = scene.dataset.scene;
     const sceneContainer = scene.querySelector(".active-collections__scene");
     const controls = scene.querySelectorAll(".active-collections__control");
+    const imageNode = scene.querySelector(".imageNode");
 
     controls.forEach((trigger) => {
       trigger.addEventListener("click", (e) => {
         controls.forEach((el) => el.classList.remove("active"));
         trigger.classList.add("active");
-        const color = trigger.dataset.color;
-        console.log({
-          sceneIndex,
-          color,
-          sceneContainer,
-        });
+        const colorIndex = trigger.dataset.color;
+        imageNode.setAttribute(
+          "src",
+          `/img/home/activeCollections/${sceneTitle}/${colorIndex}.png`
+        );
       });
     });
+    controls[0].click();
   });
 }
